eb4550a03e84760ac0b3378601e31624d1624f0c
Ernest Nezic <ernest.nezic@gmail.com>
2020-07-28 18:10:03 +0200

    Add an "Other" option to the Job Role section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
+//When the web page loads, the first text field is automatically selected, in 'focus' for editing
 document.getElementById('name').focus();
+
+document.getElementById('other-title').style.display = 'none';
+
+
