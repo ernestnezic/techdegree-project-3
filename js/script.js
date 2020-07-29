2dcfd4ec9d608c47aea0b746a461c619b7b46046
Ernest Nezic <ernest.nezic@gmail.com>
2020-07-30 00:39:40 +0200

    Payment methods added

--- js/script.js
+++ js/script.js
@@ -2,123 +2,156 @@
  * GLOBAL VARIABLES
  ******************************************************/
 
-const designPanel = document.getElementById('design');
-const jsPuns = designPanel.firstElementChild.nextElementSibling.value;
-const heartJs = designPanel.lastElementChild.value;
+    const designPanel = document.getElementById('design');
+    const jsPuns = designPanel.firstElementChild.nextElementSibling.value;
+    const heartJs = designPanel.lastElementChild.value;
 
-const colorOptions = document.getElementById('color').children;
-const colorSelector = colorOptions[0].parentNode;
+    const colorOptions = document.getElementById('color').children;
+    const colorSelector = colorOptions[0].parentNode;
 
-const activitiesSelector = document.getElementsByClassName('activities')[0];
-const activities = activitiesSelector.children;
-let totalPrice = 0;
+    const activitiesSelector = document.getElementsByClassName('activities')[0];
+    const activities = activitiesSelector.children;
+    let totalPrice = 0;
 
+    const paymentSelector = document.getElementById("payment");
+//
 
 
 /******************************************************
  * UPDATES UPPON LAUNCHING
  ******************************************************/
 
-//When the web page loads, the first text field is automatically selected, in 'focus' for editing
-document.getElementById('name').focus();
+    //When the web page loads, the first text field is automatically selected, in 'focus' for editing
+    document.getElementById('name').focus();
 
-//Hides the 'other' text field upon launching
-document.getElementById('other-title').style.display = 'none';
+    //Hides the 'other' text field upon launching
+    document.getElementById('other-title').style.display = 'none';
 
-const totalCostDisplay = document.createElement('h2');
-totalCostDisplay.textContent = 'TOTAL: $0';
-activitiesSelector.appendChild(totalCostDisplay);
-
+    const totalCostDisplay = document.createElement('h2');
+    totalCostDisplay.textContent = 'TOTAL: $0';
+    activitiesSelector.appendChild(totalCostDisplay);
+//
 
 
 /******************************************************
  * FUNCTIONS
  ******************************************************/
 
-function tshirtSection() {
-    const selectOptionElement = document.createElement('option');
-    selectOptionElement.value = 'selecttshirt';
-    selectOptionElement.textContent = 'Please select T-shirt theme';
-    
-    for (let i = 0; i < colorOptions.length; i++) {
-        colorOptions[i].style.display = 'none';
-    }
-    colorSelector.value = ''
-    colorSelector.insertBefore(selectOptionElement, colorSelector.firstElementChild);
-}
-
-
-function updateColorPanel ( trigger ) {  
-    if (colorOptions[0].value === 'selecttshirt') {
-        colorSelector.removeChild(colorSelector.firstElementChild);
-    }
-
-    if (trigger == jsPuns) {
-        for (let i = 0; i < 3; i++) {
-            colorOptions[i].style.display = 'block';
-        }
-        for (let i = 3; i < 6; i++) {
+    function tshirtSection() {
+        const selectOptionElement = document.createElement('option');
+        selectOptionElement.value = 'selecttshirt';
+        selectOptionElement.textContent = 'Please select T-shirt theme';
+        
+        for (let i = 0; i < colorOptions.length; i++) {
             colorOptions[i].style.display = 'none';
         }
-        colorSelector.options[0].selected = 'selected';
-    } else if (trigger == heartJs) {
-        for (let i = 3; i < 6; i++) {
-            colorOptions[i].style.display = 'block';
-        }
-        for (let i = 0; i < 3; i++) {
-            colorOptions[i].style.display = 'none';
-        }
-        colorSelector.options[3].selected = 'selected';
-    }  
-}
-
-
-function activityHandler ( trigger ) {
-   
-    const checkboxes = activitiesSelector.querySelectorAll('input');
-
-    if (trigger.checked === true) {
-        totalPrice += +trigger.getAttribute('data-cost');
-    } else if (trigger.checked === false) {
-        totalPrice -= +trigger.getAttribute('data-cost');
+        colorSelector.value = ''
+        colorSelector.insertBefore(selectOptionElement, colorSelector.firstElementChild);
     }
 
-    totalCostDisplay.textContent = `TOTAL: $${totalPrice}`;
+
+    function updateColorPanel ( trigger ) {  
+        if (colorOptions[0].value === 'selecttshirt') {
+            colorSelector.removeChild(colorSelector.firstElementChild);
+        }
+
+        if (trigger == jsPuns) {
+            for (let i = 0; i < 3; i++) {
+                colorOptions[i].style.display = 'block';
+            }
+            for (let i = 3; i < 6; i++) {
+                colorOptions[i].style.display = 'none';
+            }
+            colorSelector.options[0].selected = 'selected';
+        } else if (trigger == heartJs) {
+            for (let i = 3; i < 6; i++) {
+                colorOptions[i].style.display = 'block';
+            }
+            for (let i = 0; i < 3; i++) {
+                colorOptions[i].style.display = 'none';
+            }
+            colorSelector.options[3].selected = 'selected';
+        }  
+    }
+
+
+    function activityHandler ( trigger ) {
     
-    if (trigger.checked === true) {
-        for (let i = 0; i < checkboxes.length; i++) {
-            if (checkboxes[i].getAttribute('data-day-and-time') === trigger.getAttribute('data-day-and-time') && checkboxes[i].getAttribute('name') !== trigger.getAttribute('name')) {
-                checkboxes[i].disabled = true;
-            }
+        const checkboxes = activitiesSelector.querySelectorAll('input');
+
+        if (trigger.checked === true) {
+            totalPrice += +trigger.getAttribute('data-cost');
+        } else if (trigger.checked === false) {
+            totalPrice -= +trigger.getAttribute('data-cost');
         }
-    } else {
-        for (let i = 0; i < checkboxes.length; i++) {
-            if (checkboxes[i].getAttribute('data-day-and-time') === trigger.getAttribute('data-day-and-time') && checkboxes[i].getAttribute('name') !== trigger.getAttribute('name')) {
-                checkboxes[i].disabled = false;
+
+        totalCostDisplay.textContent = `TOTAL: $${totalPrice}`;
+        
+        if (trigger.checked === true) {
+            for (let i = 0; i < checkboxes.length; i++) {
+                if (checkboxes[i].getAttribute('data-day-and-time') === trigger.getAttribute('data-day-and-time') && checkboxes[i].getAttribute('name') !== trigger.getAttribute('name')) {
+                    checkboxes[i].disabled = true;
+                }
+            }
+        } else {
+            for (let i = 0; i < checkboxes.length; i++) {
+                if (checkboxes[i].getAttribute('data-day-and-time') === trigger.getAttribute('data-day-and-time') && checkboxes[i].getAttribute('name') !== trigger.getAttribute('name')) {
+                    checkboxes[i].disabled = false;
+                }
             }
         }
     }
-}
 
+
+    function paymentHandler( trigger ) {   
+        const creditCardDiv = document.getElementById('credit-card');
+        const paypalDiv = document.getElementById('paypal');
+        const bitcoinDiv = document.getElementById('bitcoin');
+
+        if (paymentSelector.firstElementChild.value === 'select method') {
+        paymentSelector.removeChild(paymentSelector[0]);
+        }
+        if ( trigger === 'paypal') {
+            creditCardDiv.hidden = true;
+            bitcoinDiv.hidden = true;
+            paypalDiv.hidden = false;
+        } else if ( trigger === 'bitcoin') {
+            creditCardDiv.hidden = true;
+            bitcoinDiv.hidden = false;
+            paypalDiv.hidden = true;
+        } else if ( trigger === 'credit card') {
+            console.log(trigger);
+            creditCardDiv.hidden = false;
+            bitcoinDiv.hidden = true;
+            paypalDiv.hidden = true;
+        }
+
+    }
+//
 
 
 /******************************************************
  * EVENT LISTENERS
  ******************************************************/
 
-//Detecting change in the design panel
-designPanel.addEventListener('change', (e) => {
-    updateColorPanel(e.target.value);
-})
+    //Detecting change in the design panel
+    designPanel.addEventListener('change', (e) => {
+        updateColorPanel(e.target.value);
+    })
 
-activitiesSelector.addEventListener('change', (e) => {
-    activityHandler(e.target);
-});
+    activitiesSelector.addEventListener('change', (e) => {
+        activityHandler(e.target);
+    });
 
+    paymentSelector.addEventListener('change', (e) =>{
+        paymentHandler(e.target.value);
+    })
+//
 
 
 /******************************************************
  * CALLING FUNCTIONS
  ******************************************************/
 
-tshirtSection();
+    tshirtSection();
+//
